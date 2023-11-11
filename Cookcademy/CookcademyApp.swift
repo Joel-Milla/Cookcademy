24a3fe94350b630c99c6cb1ead646de686ec6d7e
Joel-Milla <jalejandro.milla@outlook.com>
2023-11-11 14:04:36 -0600

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Alumno on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
