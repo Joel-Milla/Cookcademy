f9ded1f1c89265bc828ec3ae8d888e874fe33ac7
Joel-Milla <jalejandro.milla@outlook.com>
2023-11-16 18:33:44 -0600

    Add an option to select the main information of the new recipe.

---
+++ Cookcademy/Views/ModifyRecipes/ModifyMainInformationView.swift
@@ -0,0 +1,44 @@
+//
+//  ModifyMainInformationView.swift
+//  Cookcademy
+//
+//  Created by Alumno on 16/11/23.
+//
+
+import SwiftUI
+
+struct ModifyMainInformationView: View {
+    private let listBackgroundColor = AppColor.background
+    private let listTextColor = AppColor.foreground
+    
+    @Binding var mainInformation: MainInformation
+    
+    var body: some View {
+        Form {
+            TextField("Recipe Name", text: $mainInformation.name)
+                .listRowBackground(listBackgroundColor)
+            TextField("Author", text: $mainInformation.author)
+                .listRowBackground(listBackgroundColor)
+            Section(header: Text("Description")) {
+                TextEditor(text: $mainInformation.description)
+            }
+            Picker("Category", selection: $mainInformation.category) {
+                ForEach(MainInformation.Category.allCases,
+                        id: \.self) { category in
+                    Text(category.rawValue)
+                }
+            }
+            .listRowBackground(listBackgroundColor)
+            .pickerStyle(MenuPickerStyle())
+        }
+        .foregroundColor(listTextColor)
+    }
+}
+
+struct ModifyMainInformationView_Previews: PreviewProvider {
+    @State static var emptyInformation = MainInformation(name: "", description: "", author: "", category: .lunch)
+    
+    static var previews: some View {
+        ModifyMainInformationView(mainInformation: $emptyInformation)
+    }
+}
