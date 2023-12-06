5fa95f924f0b16cded02ef238b83c14aca5fb4a6
Joel-Milla <jalejandro.milla@outlook.com>
2023-12-05 18:17:38 -0600

    Add the MainView which has a TabView container that will be the central point of the application.

---
+++ Cookcademy/Views/MainTabView.swift
@@ -0,0 +1,36 @@
+//
+//  MainTabView.swift
+//  Cookcademy
+//
+//  Created by Joel Alejandro Milla Lopez on 05/12/23.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    @StateObject var recipeData = RecipeData()
+    
+    var body: some View {
+        TabView {
+            RecipeCategoryGridView()
+                .tabItem {
+                    Label("Recipes", systemImage: "list.dash")
+                }
+            NavigationView {
+                RecipesListView(category: .breakfast)
+            }
+            .tabItem {
+                Label("Favorites", systemImage: "heart")
+            }
+        }
+        .environmentObject(recipeData)
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            MainTabView()
+        }
+    }
+}
