7e917e880c87100a53586b63a5773951be9c830f
Joel-Milla <jalejandro.milla@outlook.com>
2024-01-03 12:43:46 -0600

    Add functions that save and load the recipes stored in FileManager.

--- Cookcademy/ViewModels/RecipeData.swift
+++ Cookcademy/ViewModels/RecipeData.swift
@@ -37,4 +37,34 @@ class RecipeData: ObservableObject {
         }
         return nil
     }
+    
+    private var recipesFileURL: URL {
+        do {
+            let documentsDirectory = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+            return documentsDirectory.appendingPathComponent("recipeData")
+        }
+        catch {
+            fatalError("An error occurred while getting the url of recipesFile: \(error)")
+        }
+    }
+    
+    func savesRecipes() {
+        do {
+            let encodedData = try JSONEncoder().encode(recipes)
+            try encodedData.write(to: recipesFileURL)
+        }
+        catch {
+            fatalError("An error occured while saving the recipes: \(error)")
+        }
+    }
+    
+    func loadRecipes() {
+        guard let data = try? Data(contentsOf: recipesFileURL) else { return }
+        do {
+            recipes = try JSONDecoder().decode([Recipe].self, from: data)
+        }
+        catch {
+            fatalError("An error occurred while loading the recipes: \(error)")
+        }
+    }
 }
